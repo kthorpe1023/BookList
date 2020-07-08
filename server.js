2c40fc1703bd2454306e455656b0fa0d39a3b75f
Katie Thorpe <kthorpe1023@gmail.com>
2020-07-07 20:07:01 -0600

    try again

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var GitHubStrategy = require('passport-github').Strategy;
 var passport = require('passport')
 passport.use(new GitHubStrategy({
-    clientID: "7b5235c21d2f080de6ee",
-    clientSecret: "489579aba11175e9122f5f38f6392b2131ef66dd",
+    clientID: process.env["7b5235c21d2f080de6ee"],
+    clientSecret: process.env["489579aba11175e9122f5f38f6392b2131ef66dd"],
     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -16,6 +16,8 @@ passport.use(new GitHubStrategy({
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+// define routes
 app.get('/',
   passport.authenticate('github'));
 
@@ -25,6 +27,31 @@ app.get('/auth/github/callback',
     // Successful authentication, redirect home.
     res.redirect('/');
   });
+
+  app.get('/',
+  function(req, res) {
+    res.render('index', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/github',
+  passport.authenticate('github'));
+
+// app.get('/return', 
+//   passport.authenticate('github', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
+// app.get('/profile',
+//   require('connect-ensure-login').ensureLoggedIn(),
+//   function(req, res){
+//     res.render('profile', { user: req.user });
+//   });
 
 
 // Serve static content for the app from the "public" directory in the application directory.
